0d43246320c05106169c0906b18c80f4f7938293
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-30 08:07:12 +0530

    Update roboshop-infra/cart-infra/

--- main.auto.tfvars
+++ main.auto.tfvars
@@ -1,1 +1,2 @@
 COMPONENT = "cart"
+AZ        = ["us-east-1b", "us-east-1c"]
--- main.tf
+++ main.tf
@@ -3,5 +3,7 @@ module "vpc" {
   VPC_CIDR_BLOCK = var.VPC_CIDR_BLOCK
   COMPONENT      = var.COMPONENT
   ENV            = var.ENV
+  SUBNET_CIDR    = var.SUBNET_CIDR
+  AZ             = var.AZ
 }
 
