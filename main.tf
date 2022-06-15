2f51285796a9b2824cf75808a333d2932b7673f0
Raghu K <raghuk.vit@gmail.com>
2022-06-15 08:46:45 +0530

    Update roboshop-infra/cart-infra/

--- main.tf
+++ main.tf
@@ -8,18 +8,19 @@ module "vpc" {
 }
 
 module "elasticache" {
-  depends_on     = [module.vpc]
-  source         = "git::https://github.com/raghudevopsb64/tf-module-elasticache-redis.git"
-  ENGINE         = var.ENGINE
-  ENGINE_VERSION = var.ENGINE_VERSION
-  NODE_TYPE      = var.NODE_TYPE
-  ENV            = var.ENV
-  COMPONENT      = var.COMPONENT
-  NODE_COUNT     = var.NODE_COUNT
-  FAMILY         = var.FAMILY
-  VPC_ID         = module.vpc.VPC_ID
-  VPC_CIDR       = module.vpc.VPC_CIDR
-  SUBNET_IDS     = module.vpc.SUBNET_IDS
+  depends_on             = [module.vpc]
+  source                 = "git::https://github.com/raghudevopsb64/tf-module-elasticache-redis.git"
+  ENGINE                 = var.ENGINE
+  ENGINE_VERSION         = var.ENGINE_VERSION
+  NODE_TYPE              = var.NODE_TYPE
+  ENV                    = var.ENV
+  COMPONENT              = var.COMPONENT
+  NODE_COUNT             = var.NODE_COUNT
+  FAMILY                 = var.FAMILY
+  VPC_ID                 = module.vpc.VPC_ID
+  VPC_CIDR               = module.vpc.VPC_CIDR
+  SUBNET_IDS             = module.vpc.SUBNET_IDS
+  PRIVATE_HOSTED_ZONE_ID = module.vpc.PRIVATE_HOSTED_ZONE_ID
 }
 
 module "app" {
