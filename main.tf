1422030c4cab4363f5191e9826bd1f6f77de2c17
Raghu K <raghuk.vit@gmail.com>
2022-07-06 09:09:24 +0530

    Update roboshop-infra/cart-infra/

--- main.tf
+++ main.tf
@@ -39,4 +39,7 @@ module "app" {
   VPC_ACCESS_TO_ALB      = [module.vpc.VPC_CIDR, "10.10.5.0/24", "${module.vpc.NGW_PRIVATE_IP}/32"]
   PRIVATE_HOSTED_ZONE_ID = module.vpc.PRIVATE_HOSTED_ZONE_ID
   PROMETHEUS_IP          = var.PROMETHEUS_IP
+  CAPACITY_NODES         = var.CAPACITY_NODES
+  MIN_NODES              = var.MIN_NODES
+  MAX_NODES              = var.MAX_NODES
 }
